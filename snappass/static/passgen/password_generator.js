34ac4ba61ddf7475f691f01760002bcd562f8939
imbrykin <brykinivan4@gmail.com>
2024-12-28 04:54:07 +0300

    adding checkbox for special symbols

--- snappass/static/passgen/password_generator.js
+++ snappass/static/passgen/password_generator.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
         generatePasswordButton.addEventListener('click', function () {
             const lengthField = document.getElementById('password_length');
             const passwordField = document.getElementById('password');
+            const specialSymbolsCheckbox = document.getElementById('special_symbols'); // Чекбокс для специальных символов
             
             const length = parseInt(lengthField.value) || 24;
             if (length < 1 || length > 128) {
@@ -11,7 +12,11 @@ document.addEventListener('DOMContentLoaded', function () {
                 return;
             }
 
-            const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@$%^&*()-_+=|:.';
+            let charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            if (specialSymbolsCheckbox && specialSymbolsCheckbox.checked) {
+                charset += '!@$%^&*()-_+=|:.';
+            }
+
             let password = '';
             for (let i = 0; i < length; i++) {
                 password += charset[Math.floor(Math.random() * charset.length)];
